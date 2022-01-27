391b58f7034e3ff0fc6a53aa8b917ad77517bd71
huyennguyen1406 <thanhhuyen.ubtlu@gmail.com>
2022-01-27 15:36:56 +0700

    fix

--- Minitest/Minitest2/src/main/java/controller/ProductServlet.java
+++ Minitest/Minitest2/src/main/java/controller/ProductServlet.java
@@ -46,7 +46,7 @@ public class ProductServlet extends HttpServlet {
                 cart(request, response);
                 break;
             default:
-                displayAllProduct(request, response);
+                displayAll(request, response);
         }
     }
 
@@ -88,104 +88,10 @@ public class ProductServlet extends HttpServlet {
         requestDispatcher.forward(request, response);
     }
 
-    private void displayAllProduct(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ArrayList<Product> products = productService.getProductList();
+    private void displayAll(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        ArrayList<Product> products = productService.getProducts();
         request.setAttribute("products", products);
         RequestDispatcher requestDispatcher = request.getRequestDispatcher("view.jsp");
         requestDispatcher.forward(request, response);
     }
-
-//    private void displayAll(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        ArrayList<Product> products = productService.getProducts();
-//        request.setAttribute("listProduct", products);
-//        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view.jsp");
-//        requestDispatcher.forward(request, response);
-//    }
-//
-//    private void displayDetail(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        int id = Integer.parseInt(request.getParameter("id"));
-//        Product product = productService.getProduct(id);
-//        request.setAttribute("product", product);
-//        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view-detail.jsp");
-//        requestDispatcher.forward(request, response);
-//    }
-//
-//    private void createGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        ArrayList<String> brands = productService.getBrands();
-//        request.setAttribute("brand", brands);
-//        RequestDispatcher requestDispatcher = request.getRequestDispatcher("create.jsp");
-//        requestDispatcher.forward(request, response);
-//    }
-//
-//    private void createPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        int id = Integer.parseInt(request.getParameter("id"));
-//        String name = request.getParameter("name");
-//        double price = Double.parseDouble(request.getParameter("price"));
-//        int quantity = Integer.parseInt(request.getParameter("quantity"));
-//        String description = request.getParameter("description");
-//        productService.createProduct(new Product(id, name, price, quantity, description));
-//        ArrayList<Product> products = productService.getProducts();
-//        request.setAttribute("listProduct", products);
-//        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view.jsp");
-//        requestDispatcher.forward(request, response);
-//    }
-//
-//    private void editGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        int id = Integer.parseInt(request.getParameter("id"));
-//        Product product = productService.getProduct(id);
-//        ArrayList<String> brands = product.getBrands();
-//        request.setAttribute("brand", brands);
-//        request.setAttribute("product", product);
-//        RequestDispatcher requestDispatcher = request.getRequestDispatcher("create.jsp");
-//        requestDispatcher.forward(request, response);
-//    }
-//
-//    private void sort(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        ArrayList<Product> products = productService.sortProduct();
-//        request.setAttribute("products", products);
-//        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view.jsp");
-//        requestDispatcher.forward(request, response);
-//    }
-//
-//    private void editPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        ArrayList<Product> products = productService.getProducts();
-//        int id = Integer.parseInt(request.getParameter("id"));
-//        String name = request.getParameter("name");
-//        double price = Double.parseDouble(request.getParameter("price"));
-//        int quantity = Integer.parseInt(request.getParameter("quantity"));
-//        String description = request.getParameter("description");
-//        Product product = productService.getProduct(id);
-//        int index = -1;
-//        for (int i = 0; i < products.size(); i++) {
-//            if (products.get(i).getId() == id) {
-//                index = i;
-//            }
-//        }
-//        product.editProduct(new Product(id, name, price, quantity, description), index);
-//        products = productService.getProducts();
-//        request.setAttribute("listProduct", products);
-//        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view.jsp");
-//        requestDispatcher.forward(request, response);
-//    }
-//
-//    private void delete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-//        int id = Integer.parseInt(request.getParameter("id"));
-//        Product product = productService.getProduct(id);
-//        product.deleteProduct(product);
-//        ArrayList<Product> products = productService.getProducts();
-//        request.setAttribute("listProduct", products);
-//        RequestDispatcher requestDispatcher = request.getRequestDispatcher("view.jsp");
-//        requestDispatcher.forward(request, response);
-//    }
-//
-//    public ArrayList<Product> sortProductListAsc() {
-//        products.sort((o1, o2) -> (int) (o1.getPrice() - o2.getPrice()));
-//        return products;
-//    }
-//
-//    public ArrayList<Product> sortProductListDesc() {
-//        products.sort((o1, o2) -> (int) (o2.getPrice() - o1.getPrice()));
-//        return products;
-//    }
-
 }
